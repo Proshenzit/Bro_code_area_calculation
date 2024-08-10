dd4435a0a0e92112070a0c280c8b98dcecd7b457
Proshenzit Roy <37993972+Proshenzit@users.noreply.github.com>
2024-08-10 05:08:59 -0700

    Add files via upload

---
+++ bro_code_area_calculation.py
@@ -0,0 +1,5 @@
+length =float(input("Enter   the length of rectangle: "))
+width  =float(input("Enter   the width of rectangle:"))
+height = float(input("Enter   the height  of   height :"))
+vloume  = length*width*height
+print(f" The area  is{vloume}cm^2")
